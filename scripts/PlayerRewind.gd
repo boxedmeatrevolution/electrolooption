e1e2e778c96219578bf16af068b9f4e4b8645e1d
Duane Byer <duane.byer@gmail.com>
2020-10-03 19:22:46 -0400

    LIghtninggit add *git add *

--- scripts/PlayerRewind.gd
+++ scripts/PlayerRewind.gd
@@ -2,16 +2,25 @@ extends Node2D
 
 const GameState := preload("res://scripts/GameState.gd")
 
+onready var lightning := $Lightning
 onready var area := $Area2D
 
 var game_state : GameState
 var idx : int
+var next : Node2D
 
 func setup(game_state: GameState):
 	self.game_state = game_state
 	self.idx = game_state.get_past_player_pos().size() - 1
 	self.game_state.connect("on_player_rewind", self, "_rewind")
 	position = Utility.board_to_world(self.game_state.get_past_player_pos()[idx])
+
+func _process(delta : float) -> void:
+	var past := game_state.get_past_player_pos()
+	if past.size() - 1 == idx:
+		lightning.target = Utility.board_to_world(game_state.get_player_pos())
+	else:
+		lightning.target = Utility.board_to_world(past[idx + 1])
 
 func _rewind(idx: int) -> void:
 	# On rewind, if this no longer exists, then destroy it.
